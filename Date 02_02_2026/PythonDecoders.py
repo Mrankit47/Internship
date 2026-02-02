7edbd2d53784751f3427c407edd762250d88d50d
Ankit Kushwah <ankitkushwah0210k@gmail.com>
2026-02-02 17:15:01 +0530

    Create PythonDecoders.py

---
+++ Date 02_02_2026/PythonDecoders.py
@@ -0,0 +1,8 @@
+''' A decorator is a function that 
+    1. Takes another function as input
+    2. add extra behavior to it 
+    3. without modifying the origional funcitn'''
+
+# in simple  Decorator = function that wraps another function
+
+
